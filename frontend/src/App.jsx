2de34b199177b837e233aab1fa78064443241545
Marie-Sail <marie.saillard.pro@gmail.com>
2023-12-21 12:51:42 +0100

    creation Recipe Card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import RecipeCard from "./components/RecipeCard/RecipeCard";
 
 function App() {
   return (
     <div className="App">
+      <RecipeCard />
       <Outlet />
       <Navbar />
     </div>
