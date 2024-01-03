a77541423ef05c751e638b23416f61a0f3b212d1
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-01-03 17:25:12 +0100

    add the map and display of both route to filter recipe by tags 1 and 2

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -4,17 +4,38 @@ import RecipeCard from "../../components/RecipeCard/RecipeCard";
 
 function Homepage() {
   const [recipe, setRecipe] = useState();
+  const [vegan, setVegan] = useState();
+  const [mexican, setMexican] = useState();
   useEffect(() => {
     fetch(`${import.meta.env.VITE_BACKEND_URL}/api/randomrecipe`)
       .then((response) => response.json())
       .then((data) => setRecipe(data))
       .catch((error) => console.error(error));
+
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/api/tag/1`)
+      .then((response) => response.json())
+      .then((data) => setVegan(data))
+      .catch((error) => console.error(error));
+
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/api/tag/2`)
+      .then((response) => response.json())
+      .then((data) => setMexican(data))
+      .catch((error) => console.error(error));
   }, []);
 
   return (
     <>
-      {recipe ? <RecipeCard recipe={recipe} /> : "null"};
-      <h1 className="helloHome">HELLO HOMEPAGE</h1>;
+      {recipe ? <RecipeCard recipe={recipe} /> : "loading"}
+      {vegan
+        ? vegan.map((veganRecipe) => (
+            <RecipeCard key={recipe.id} recipe={veganRecipe} />
+          ))
+        : "loading"}
+      {mexican
+        ? mexican.map((mexicanRecipe) => (
+            <RecipeCard key={recipe.id} recipe={mexicanRecipe} />
+          ))
+        : "loading"}
     </>
   );
 }
