24d81881d96473fa96a41599fda4919a77a494e5
FREDERIC TICON <fred.ticon@gmail.com>
2024-01-03 16:28:49 +0100

    Update TitleBar.jsx

--- frontend/src/components/TitleBar/TitleBar.jsx
+++ frontend/src/components/TitleBar/TitleBar.jsx
@@ -10,7 +10,7 @@ function TitleBar() {
         src="src/assets/Logo-nam-nam.png"
         alt="logo"
       />
-      <Link to="/" className="titleBar__userLogo" type="submit">
+      <Link to="/userprofil" className="titleBar__userLogo" type="submit">
         <img name="userLogo" src="src/assets/icone-user.svg" alt="userLogo" />
       </Link>
     </header>
