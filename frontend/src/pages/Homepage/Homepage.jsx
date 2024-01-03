0bea76fd913519382182905439d5e1d8e7fcc446
ROBIN FANTINO <robin.fantino.rf@gmail.com>
2024-01-03 13:32:48 +0100

    i did the swiper component

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -1,11 +1,13 @@
 import "./Homepage.scss";
 import RecipeCard from "../../components/RecipeCard/RecipeCard";
+import Swipper from "../../components/Swipper/Swipper";
 
 function Homepage() {
   return (
     <>
       <RecipeCard />
       <h1 className="helloHome">HELLO HOMEPAGE</h1>;
+      <Swipper />
     </>
   );
 }
