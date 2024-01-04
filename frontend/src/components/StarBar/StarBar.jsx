780910176ba4e6347b4380f670656c5479437aa2
Marie-Sail <marie.saillard.pro@gmail.com>
2024-01-04 13:33:38 +0100

    remove commments StarBar

--- frontend/src/components/StarBar/StarBar.jsx
+++ frontend/src/components/StarBar/StarBar.jsx
@@ -1,28 +1,10 @@
 import "./StarBar.scss";
 
-// import React, { useState } from "react";
-// import { Rating } from "react-simple-star-rating";
-
 function StarBar() {
   const gradesRecipe = 4;
 
   const fullStars = Array(5).fill("src/assets/star.svg");
   const emptyStars = Array(5).fill("src/assets/emptyStar.svg");
-
-  // const [rating, setRating] = useState(0);
-
-  // const handleRating = (rate) => {
-  //   console.log(rate);
-  //   setRating(rate);
-  // };
-
-  // ///////////////////////////////////////
-  // const gradesRecipe = [2, 1, 3, 3];
-
-  // function sumGrades(accumulator, value) {
-  //   return accumulator + value;
-  // }
-  // const mediumGrades = gradesRecipe.reduce(sumGrades) / gradesRecipe.length;
 
   return (
     <div>
@@ -34,62 +16,6 @@ function StarBar() {
         return <img src={emptyStar} alt="empty star" />;
       })}
     </div>
-    // <div className="App">
-    //   <Rating onClick={handleRating} ratingValue={rating} allowFraction />
-    //   <Rating />
-    // </div>
-    //    <img src="src/assets/star.svg" alt="star" />
-    //   <img src="src/assets/star.svg" alt="star" />
-    //    <img src="src/assets/star.svg" alt="star" />
-    //    <img src="src/assets/star.svg" alt="star" />
-    //    <img src="src/assets/emptyStar.svg" alt="empty star" />
-    //   {" "}
-    // </div>
-    // /////////////////////////////////////////////////////////////////////////
-    //     { mediumGrades <= 1 ? <div>
-    //       <img src="src/assets/star.svg" alt="star" />
-    //       <img src="src/assets/emptyStar.svg" alt="empty star" />
-    //       <img src="src/assets/emptyStar.svg" alt="empty star" />
-    //       <img src="src/assets/emptyStar.svg" alt="empty star" />
-    //       <img src="src/assets/emptyStar.svg" alt="empty star" />
-    //     </div> :
-    //     mediumGrades <=2 ? <div>
-    //     <img src="src/assets/star.svg" alt="star" />
-    //     <img src="src/assets/star.svg" alt="star" />
-    //     <img src="src/assets/emptyStar.svg" alt="empty star" />
-    //     <img src="src/assets/emptyStar.svg" alt="empty star" />
-    //     <img src="src/assets/emptyStar.svg" alt="empty star" />
-    //   </div> :
-    //   mediumGrades <=3 ?
-    //   <div>
-    //   <img src="src/assets/star.svg" alt="star" />
-    //   <img src="src/assets/star.svg" alt="star" />
-    //   <img src="src/assets/star.svg" alt="star" />
-    //   <img src="src/assets/emptyStar.svg" alt="empty star" />
-    //   <img src="src/assets/emptyStar.svg" alt="empty star" />
-    // </div> :
-    // mediumGrades <= 4 ?   <div>
-    // <img src="src/assets/star.svg" alt="star" />
-    // <img src="src/assets/star.svg" alt="star" />
-    // <img src="src/assets/star.svg" alt="star" />
-    // <img src="src/assets/star.svg" alt="star" />
-    // <img src="src/assets/emptyStar.svg" alt="empty star" />
-    // </div> :
-    // mediumGrades <= 5 ?   <div>
-    // <img src="src/assets/star.svg" alt="star" />
-    // <img src="src/assets/star.svg" alt="star" />
-    // <img src="src/assets/star.svg" alt="star" />
-    // <img src="src/assets/star.svg" alt="star" />
-    // <img src="src/assets/star.svg" alt="star" />
-    // </div> :
-    // <div>
-    // <img src="src/assets/emptyStar.svg" alt="empty star" />
-    // <img src="src/assets/emptyStar.svg" alt="empty star" />
-    // <img src="src/assets/emptyStar.svg" alt="empty star" />
-    // <img src="src/assets/emptyStar.svg" alt="empty star" />
-    // <img src="src/assets/emptyStar.svg" alt="empty star" />
-    // </div>
-    //   }
   );
 }
 export default StarBar;
