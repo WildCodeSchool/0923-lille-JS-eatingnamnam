7619ebfaf1700df6817f6ddc8fa86aaa5abafc63
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-01-04 11:19:16 +0100

    remove unecesssary comments

--- backend/src/models/ListTagsRecipeManager.js
+++ backend/src/models/ListTagsRecipeManager.js
@@ -1,4 +1,4 @@
-/* const AbstractManager = require("./AbstractManager");
+const AbstractManager = require("./AbstractManager");
 
 class RecipeManager extends AbstractManager {
   constructor() {
@@ -6,17 +6,6 @@ class RecipeManager extends AbstractManager {
     // and pass the table name "item" as configuration
     super({ table: "list_tags_recipe" });
   }
-
-  async recipeByTag(id) {
-    // Execute the SQL SELECT query to retrieve all items from the "item" table
-    const [rows] = await this.database.query(
-      `SELECT * FROM ${this.table} WHERE tag_id = ?`,
-      [id]
-    );
-    // Return the array of items
-    return rows;
-  }
 }
 
 module.exports = RecipeManager;
- */
