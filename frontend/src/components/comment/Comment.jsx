d48adbd843641fa1710ac6c0b1555a503a475e52
FREDERIC TICON <fred.ticon@gmail.com>
2024-01-09 11:07:16 +0100

    Update Comment.jsx

--- frontend/src/components/comment/Comment.jsx
+++ frontend/src/components/comment/Comment.jsx
@@ -1,6 +1,32 @@
 import "./Comment.scss";
+import StarBar from "../StarBar/StarBar";
 
 function Comment() {
-  return <thecomment className="thecomment" />;
+  return (
+    <div className="thecomment">
+      <div className="flex-container">
+        <div className="flex-items">
+          <img
+            className="thecomment__userLogo__img"
+            name="userLogo"
+            src="src/assets/icone-user.svg"
+            alt="userLogo"
+          />
+        </div>
+
+        <div className="flex-items">
+          <h2 className="thecomment__user__name">userName</h2>
+          <StarBar />
+        </div>
+      </div>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam veniam
+        incidunt delectus distinctio a sapiente, numquam sit ducimus
+        exercitationem ratione. Fugiat iusto cum optio delectus amet deserunt
+        quas maiores natus!
+      </p>
+      <p className="thecomment__date">2023-12-25 - 17h30</p>
+    </div>
+  );
 }
 export default Comment;
