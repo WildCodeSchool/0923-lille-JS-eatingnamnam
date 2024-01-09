594c4a0dc2354b38cee32de39b56bf7f5d3cbffe
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-01-09 16:27:03 +0100

    add navBar button component to switch between ustensil, prepsteps and ingredients

--- frontend/src/pages/Recipe/Recipe.jsx
+++ frontend/src/pages/Recipe/Recipe.jsx
@@ -5,6 +5,10 @@ import RecipeSteps from "../../components/RecipeSteps/RecipeSteps";
 
 function Recipe() {
   const [recipe, setRecipe] = useState();
+  const [tab, setTab] = useState("1");
+  const [ingredientIsActive, setIngredientIsActive] = useState(1);
+  const [ustensilIsActive, setUstensilIsActive] = useState(0);
+  const [stepIsActive, setStepIsActive] = useState(0);
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_BACKEND_URL}/api/recipe/1`)
@@ -12,20 +16,78 @@ function Recipe() {
       .then((data) => setRecipe(data))
       .catch((error) => console.error(error));
   }, []);
-
+  const handleCLickIngredient = () => {
+    setTab(1);
+    setIngredientIsActive(1);
+    setUstensilIsActive(0);
+    setStepIsActive(0);
+  };
+  const handleClickUstensil = () => {
+    setTab(2);
+    setIngredientIsActive(0);
+    setUstensilIsActive(1);
+    setStepIsActive(0);
+  };
+  const handleClickSteps = () => {
+    setTab(3);
+    setIngredientIsActive(0);
+    setUstensilIsActive(0);
+    setStepIsActive(1);
+  };
   return (
-    <div className="recipe__cardContainer">
+    <div>
       {recipe ? <RecipeCardPage recipe={recipe[0]} /> : "loading"}
-      <main className="recipe__stepContainer">
-        {recipe
+
+      <main className="recipe__cardContainer">
+        <nav className="recipe__buttonBar">
+          <button
+            type="submit"
+            className={
+              ingredientIsActive ? "button__Focus" : "recipe__navButton"
+            }
+            onClick={handleCLickIngredient}
+          >
+            Ingrédients
+          </button>
+          <button
+            type="submit"
+            className={ustensilIsActive ? "button__Focus" : "recipe__navButton"}
+            onClick={handleClickUstensil}
+          >
+            Ustensiles
+          </button>
+          <button
+            type="submit"
+            className={stepIsActive ? "button__Focus" : "recipe__navButton"}
+            onClick={handleClickSteps}
+          >
+            Préparation
+          </button>
+        </nav>
+        {tab === 3 && recipe
           ? recipe.map((step) => (
               <RecipeSteps
                 recipeStep={step.description}
                 stepNumber={step.number_step}
               />
             ))
-          : "no steps found"}
+          : ""}
       </main>
+
+      {tab === 3 ? (
+        <article className="recipe__stepContainer">
+          {recipe
+            ? recipe.map((step) => (
+                <RecipeSteps
+                  recipeStep={step.description}
+                  stepNumber={step.number_step}
+                />
+              ))
+            : "no steps found"}
+        </article>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
