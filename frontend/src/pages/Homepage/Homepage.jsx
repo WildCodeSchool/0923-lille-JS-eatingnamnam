13bb8164642b8c4891df2f66a7630562db7b5a49
FREDERIC TICON <fred.ticon@gmail.com>
2024-01-09 16:16:08 +0100

    Update Homepage.jsx

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -2,7 +2,7 @@ import "./Homepage.scss";
 import { useEffect, useState } from "react";
 import RecipeCard from "../../components/RecipeCard/RecipeCard";
 import Swipper from "../../components/Swipper/Swipper";
-import Comment9 from "../../components/Comment/Comment";
+import CommentCard from "../../components/CommentCard/CommentCard";
 
 function Homepage() {
   const [recipe, setRecipe] = useState();
@@ -32,7 +32,7 @@ function Homepage() {
       {vegan ? <Swipper recipes={vegan} /> : "loading"}
       <h2 className="titleHome">Recettes mexicaine</h2>
       {mexican ? <Swipper recipes={mexican} /> : "loading"}
-      <Comment9 />
+      <CommentCard />
     </>
   );
 }
