9acfe70076e9090bfe71ad09783e835f963264fa
FREDERIC TICON <fred.ticon@gmail.com>
2024-01-11 15:57:05 +0100

    Update UserProfil.jsx

--- frontend/src/pages/UserProfil/UserProfil.jsx
+++ frontend/src/pages/UserProfil/UserProfil.jsx
@@ -3,7 +3,12 @@ import "./UserProfil.scss";
 function UserProfil() {
   return (
     <>
-      <h1 className="UserProfil">USERPROFIL</h1>;
+      <img
+        className="UserProfil__img"
+        src="src/assets/PlateauFromageProfil.jpg"
+        alt="a magnifying glass icon"
+      />
+      <h1 className="UserProfil__name">User Profil</h1>;
     </>
   );
 }
