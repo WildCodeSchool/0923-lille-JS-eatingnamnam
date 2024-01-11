38329410baa8c6da79d217ee2bc8a25f8f73fd0c
Marie-Sail <marie.saillard.pro@gmail.com>
2024-01-11 10:47:28 +0100

    adjustment Home

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -13,25 +13,32 @@ function Homepage() {
       .then((data) => setRecipe(data))
       .catch((error) => console.error(error));
 
-    fetch(`${import.meta.env.VITE_BACKEND_URL}/api/tag/1`)
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/api/recipe`)
       .then((response) => response.json())
       .then((data) => setVegan(data))
       .catch((error) => console.error(error));
 
-    fetch(`${import.meta.env.VITE_BACKEND_URL}/api/tag/2`)
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/api/recipe`)
       .then((response) => response.json())
       .then((data) => setMexican(data))
       .catch((error) => console.error(error));
   }, []);
 
   return (
-    <>
-      {recipe ? <RecipeCard recipe={recipe} /> : "loading"}
-      <h2 className="titleHome">Recettes vegan</h2>
-      {vegan ? <Swipper recipes={vegan} /> : "loading"}
-      <h2 className="titleHome">Recettes mexicaine</h2>
-      {mexican ? <Swipper recipes={mexican} /> : "loading"}
-    </>
+    <main className="home">
+      <section className="home__recipeDay">
+        <h1 className="home__recipeDay__title">Recette du jour</h1>
+        {recipe ? <RecipeCard recipe={recipe} /> : "loading"}
+      </section>
+      <section className="home__Theme">
+        <h2 className="home__Theme__title">Recettes vegan</h2>
+        {vegan ? <Swipper recipes={vegan} /> : "loading"}
+      </section>
+      <section className="home__Theme">
+        <h3 className="home__Theme__title">Recettes mexicaine</h3>
+        {mexican ? <Swipper recipes={mexican} /> : "loading"}
+      </section>
+    </main>
   );
 }
 
