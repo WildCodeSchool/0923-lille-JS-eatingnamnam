cc776d97f52109319dc9482e17fd6e4acffd84cb
ROBIN FANTINO <robin.fantino.rf@gmail.com>
2024-01-11 14:27:03 +0100

    add login page

---
+++ frontend/src/pages/Authentication/Authentication.jsx
@@ -0,0 +1,13 @@
+import "./Authentication.scss";
+
+function Authentication() {
+  return (
+    <img
+      className="loginBackground"
+      src="src/assets/loginBackround.svg"
+      alt=""
+    />
+  );
+}
+
+export default Authentication;
