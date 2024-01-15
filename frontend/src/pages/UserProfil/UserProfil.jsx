dcad6ada24bf4d0f5b84612c1f22a0a00ee0d38b
FREDERIC TICON <fred.ticon@gmail.com>
2024-01-15 15:56:21 +0100

    Update UserProfil.jsx

--- frontend/src/pages/UserProfil/UserProfil.jsx
+++ frontend/src/pages/UserProfil/UserProfil.jsx
@@ -9,60 +9,60 @@ function UserProfil() {
         alt="a magnifying glass icon"
       />
       <h1 className="UserProfil__name">User Profil</h1>
-      <h4 className="UserProfil__myInfo">Mes infos</h4>
+      <section className="test">
+        <h4 className="UserProfil__myInfo">Mes infos</h4>
 
-      <form className="UserProfil__info" action="#" method="post">
-        <div className="UserProfil__pseudo">
-          <label className="" htmlFor="pseudo">
-            Pseudo:
-          </label>
-          <input
-            id="pseudo"
-            className="UserProfil__pseudo__input"
-            type="text"
-          />
-        </div>
-        <div className="UserProfil__anniversaire">
-          <label className="" htmlFor="anniversaire">
-            Anniversaire:
-          </label>
-          <input
-            id="anniversaire"
-            className="UserProfil__anniversaire__input"
-            type="text"
-          />
-        </div>
-        <div className="UserProfil__nom">
-          <label className="" htmlFor="nom">
-            Nom:
-          </label>
-          <input id="nom" className="UserProfil__nom__input" type="text" />
-        </div>
-        <div className="UserProfil__prenom">
-          <label className="" htmlFor="prenom">
-            Prénom:
-          </label>
-          <input
-            id="prenom"
-            className="UserProfil__prenom__input"
-            type="text"
-          />
-        </div>
-        <div className="UserProfil__mail">
-          <label className="" htmlFor="email">
-            Email:
-          </label>
-          <input id="email" className="UserProfil__mail__input" type="text" />
-        </div>
-      </form>
+        <form className="UserProfil__info" action="#" method="post">
+          <div className="UserProfil__pseudo">
+            <label className="" htmlFor="pseudo">
+              Pseudo:
+              <input
+                id="pseudo"
+                className="UserProfil__pseudo__input"
+                type="text"
+              />
+            </label>
+          </div>
+
+          <div className="UserProfil__anniversaire">
+            <label className="" htmlFor="anniversaire">
+              Anniversaire:
+            </label>
+            <input
+              id="anniversaire"
+              className="UserProfil__anniversaire__input"
+              type="text"
+            />
+          </div>
+          <div className="UserProfil__nom">
+            <label className="testLabel" htmlFor="nom">
+              Nom:
+              <input id="nom" className="UserProfil__nom__input" type="text" />
+            </label>
+          </div>
+          <div className="UserProfil__prenom">
+            <label className="" htmlFor="prenom">
+              Prénom:
+            </label>
+            <input
+              id="prenom"
+              className="UserProfil__prenom__input"
+              type="text"
+            />
+          </div>
+          <div className="UserProfil__mail">
+            <label className="" htmlFor="email">
+              Email:
+            </label>
+            <input id="email" className="UserProfil__mail__input" type="text" />
+          </div>
+        </form>
+      </section>
       <button className="UserProfil__button__post" type="button">
         Créer un post +
       </button>
-      <h1>Mes postes</h1>
-      <button className="UserProfil__button__favoris" type="button">
-        Tous mes Favoris
-      </button>
-      <h1>Mes favorits</h1>
+      <h1 className="UserProfil__post__fav">Mes postes</h1>
+      <h1 className="UserProfil__post__fav">Mes favorits</h1>
     </>
   );
 }
