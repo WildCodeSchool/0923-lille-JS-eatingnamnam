b176d991d0bde2f7d5bfe240577575196a571ef5
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-01-15 15:26:36 +0100

    add working form

--- frontend/src/pages/Search/Search.jsx
+++ frontend/src/pages/Search/Search.jsx
@@ -1,9 +1,14 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import TagCard from "../../components/TagCard/TagCard";
 import "./Search.scss";
 
 function Search() {
   const [tags, setTags] = useState();
+  const [stepCount, setStepCount] = useState(1);
+  const [stepsArr, setMyData] = useState([]);
+
   useEffect(() => {
     fetch(`${import.meta.env.VITE_BACKEND_URL}/api/tag`)
       .then((response) => response.json())
@@ -11,8 +16,45 @@ function Search() {
       .catch((error) => console.error(error));
   }, []);
 
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitSuccessful },
+  } = useForm();
+
+  const onSubmit = (data, event) => {
+    event.preventDefault();
+    setStepCount(stepCount + 1);
+    const stepObj = { id: stepCount, desc: data.stepDescription };
+    if (!stepsArr) {
+      setMyData(stepsArr);
+    } else {
+      setMyData([...stepsArr, stepObj]);
+    }
+    console.warn(stepsArr);
+  };
+  useEffect(() => {
+    if (isSubmitSuccessful) reset();
+  }, [isSubmitSuccessful, reset]);
+
   return (
     <>
+      {stepsArr.map((step) => (
+        <p key={step.id}>
+          etape{step.id}: {step.desc}
+        </p>
+      ))}
+      <h1>step {stepCount} </h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="step description"
+          placeholder="décrivez l'étape"
+          {...register("stepDescription")}
+        />
+
+        <input type="submit" />
+      </form>
       <form className="searchBar">
         <input
           className="searchBar__form"
@@ -32,7 +74,7 @@ function Search() {
         {tags
           ? tags.map((tag) => <TagCard key={tag.id} tag={tag} />)
           : "loading"}
-      </section>
+      </section>{" "}
     </>
   );
 }
