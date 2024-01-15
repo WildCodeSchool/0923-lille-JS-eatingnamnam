b9ec3c837e0a5ae32ef3a15575d4fc5f571dcb43
FREDERIC TICON <fred.ticon@gmail.com>
2024-01-15 11:32:41 +0100

    Update UserProfil.jsx

--- frontend/src/pages/UserProfil/UserProfil.jsx
+++ frontend/src/pages/UserProfil/UserProfil.jsx
@@ -11,30 +11,50 @@ function UserProfil() {
       />
       <h1 className="UserProfil__name">User Profil</h1>
       <h4 className="UserProfil__myInfo">Mes infos</h4>
+
       <form className="UserProfil__info" action="#" method="post">
-        <label className="UserProfil__pseudo__text" htmlFor="pseudo">
-          Pseudo:
-        </label>
-        <input className="UserProfil__pseudo__input" type="text" />
-        <label
-          className="UserProfil__anniversaire__text"
-          htmlFor="anniversaire"
-        >
-          Anniversaire:
-        </label>
-        <input className="UserProfil__anniversaire__input" type="text" />
-        <label className="UserProfil__nom__text" htmlFor="nom">
-          Nom:
-        </label>
-        <input className="UserProfil__nom__input" type="text" />
-        <label className="UserProfil__prenom__text" htmlFor="prenom">
-          Prénom:
-        </label>
-        <input className="UserProfil__prenom__input" type="text" />
-        <label className="UserProfil__mail__text" htmlFor="email">
-          Email:
-        </label>
-        <input className="UserProfil__mail__input" type="text" />
+        <div className="UserProfil__pseudo">
+          <label className="" htmlFor="pseudo">
+            Pseudo:
+          </label>
+          <input
+            id="pseudo"
+            className="UserProfil__pseudo__input"
+            type="text"
+          />
+        </div>
+        <div className="UserProfil__anniversaire">
+          <label className="" htmlFor="anniversaire">
+            Anniversaire:
+          </label>
+          <input
+            id="anniversaire"
+            className="UserProfil__anniversaire__input"
+            type="text"
+          />
+        </div>
+        <div className="UserProfil__nom">
+          <label className="" htmlFor="nom">
+            Nom:
+          </label>
+          <input id="nom" className="UserProfil__nom__input" type="text" />
+        </div>
+        <div className="UserProfil__prenom">
+          <label className="" htmlFor="prenom">
+            Prénom:
+          </label>
+          <input
+            id="prenom"
+            className="UserProfil__prenom__input"
+            type="text"
+          />
+        </div>
+        <div className="UserProfil__mail">
+          <label className="" htmlFor="email">
+            Email:
+          </label>
+          <input id="email" className="UserProfil__mail__input" type="text" />
+        </div>
       </form>
       <button className="UserProfil__button__post" type="button">
         Créer un post +
