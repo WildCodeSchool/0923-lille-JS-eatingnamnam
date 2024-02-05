48462df76375f15f85c302cecf20bfe11c6c836a
Marie-Sail <marie.saillard.pro@gmail.com>
2024-02-05 13:49:09 +0100

    CSS titleBar and SearchRecipe

--- frontend/src/components/TitleBar/TitleBar.jsx
+++ frontend/src/components/TitleBar/TitleBar.jsx
@@ -1,15 +1,10 @@
 import "./TitleBar.scss";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
-import { NavContext } from "../Contexts/navBarContext";
 import { UserContext } from "../Contexts/userContext";
 
 function TitleBar() {
-  const { setActiveButton } = useContext(NavContext);
   const { auth } = useContext(UserContext);
-  const handleClick = (event) => {
-    setActiveButton(event.target.name);
-  };
 
   return (
     <header className="titleBar">
@@ -22,30 +17,17 @@ function TitleBar() {
           }/assets/images/Logo-nam-nam.png`}
           alt="logo"
         />
-        <form className="titleBar__searchBar">
-          <input
-            className="titleBar__searchBar__form"
-            type="search"
-            placeholder="Rechercher une recette..."
-            label="Rechercher une recette..."
-          />
-          <button className="titleBar__searchBar__searchButton" type="submit">
-            <img
-              className="titleBar__searchBar__searchIcon"
-              src="src/assets/activeSearch.svg"
-              alt="a magnifying glass icon"
-            />
-          </button>
-        </form>
         {auth.isLogged === false ? (
           <Link
             to="/login"
             name="login"
             className="titleBar__userLogo"
             type="submit"
-            onClick={handleClick}
           >
             <div className="titleBar__userLogo__container">
+              <h2 className="titleBar__userLogo__container__title">
+                Connection
+              </h2>
               <img
                 className="titleBar__userLogo__img"
                 name="userLogo"
@@ -54,7 +36,6 @@ function TitleBar() {
                 }/assets/images/User.png`}
                 alt="userLogo"
               />
-              <h2>Bonjour</h2>
             </div>
           </Link>
         ) : (
@@ -63,18 +44,19 @@ function TitleBar() {
             className="titleBar__userLogo"
             type="submit"
             name="profile"
-            onClick={handleClick}
           >
             <div className="titleBar__userLogo__container">
+              <h2 className="titleBar__userLogo__container__title">
+                Bonjour {auth.pseudo}
+              </h2>
               <img
-                className="titleBar__userLogo__img"
+                className="titleBar__userLogo__img__connect"
                 name="userLogo"
                 src={`${
                   import.meta.env.VITE_BACKEND_URL
                 }/assets/images/User.png`}
                 alt="userLogo"
               />
-              <h2>Bonjour {auth.pseudo}</h2>
             </div>
           </Link>
         )}
