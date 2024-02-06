86cae1c4a2bda5aec9c9d8a5ae747023df4a78b1
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-02-06 15:29:29 +0100

    add a navigate to home page on namnam logo

--- frontend/src/components/TitleBar/TitleBar.jsx
+++ frontend/src/components/TitleBar/TitleBar.jsx
@@ -1,22 +1,32 @@
 import "./TitleBar.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../Contexts/userContext";
 
 function TitleBar() {
   const { auth } = useContext(UserContext);
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/");
+  };
 
   return (
     <header className="titleBar">
       <section className="titleBar__search">
-        <img
-          className="titleBar__logo"
-          name="logo"
-          src={`${
-            import.meta.env.VITE_BACKEND_URL
-          }/assets/images/Logo-nam-nam.png`}
-          alt="logo"
-        />
+        <button
+          className="titleBar__button-navigate"
+          type="button"
+          onClick={handleClick}
+        >
+          <img
+            className="titleBar__logo"
+            name="logo"
+            src={`${
+              import.meta.env.VITE_BACKEND_URL
+            }/assets/images/Logo-nam-nam.png`}
+            alt="logo"
+          />
+        </button>
         {auth.isLogged === false ? (
           <Link
             to="/login"
