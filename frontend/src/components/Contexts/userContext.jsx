17b5e50b7261529cf9194224fb8e36c8b4aafd1c
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-02-23 10:50:24 +0100

    fix eslint

--- frontend/src/components/Contexts/userContext.jsx
+++ frontend/src/components/Contexts/userContext.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies */
+// eslint-disable-next-line import/no-extraneous-dependencies
 import axios from "axios";
 import { createContext, useState, useEffect, useMemo } from "react";
 import PropTypes from "prop-types";
